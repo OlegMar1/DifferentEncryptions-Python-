f2d6b92893a43a3332a5114aebc002657eb5d6f2
OlegMar1 <54880893+OlegMar1@users.noreply.github.com>
2020-10-12 15:59:20 +0300

    1. compleate lab-2

---
+++ lab-2/files/AZI_lab_2.py
@@ -0,0 +1,39 @@
+from collections import defaultdict
+from table import fill_table
+from progress_bar import printCompleatedProgressBar
+import random
+
+
+def encrypt_text(path_in, path_out):
+    table = fill_table()
+    rezult = ''
+    input_file = open(path_in, "r")
+    output_file = open(path_out, "w")
+
+    for line in input_file:
+        for letter in line:
+            l = letter.strip()
+            if(l == ""):
+                l = " "
+            i = random.randint(0,len(table[l])-1)
+            #print(len(table[letter.strip()]))
+            rezult += table[l][0]
+            #print(rezult)
+             
+    #print(rezult)
+    output_file.write(rezult)
+    output_file.close()
+    input_file.close()
+
+
+# ==============MAIN PART==============
+
+input_file = input("Enter input file name: ")
+output_file = input("Enter output file name: ")
+
+print("\nEncrypting file...")
+
+printCompleatedProgressBar()
+encrypt_text(input_file, output_file)
+
+print("Encrypting compleated! ")
